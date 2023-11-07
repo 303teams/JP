6efb689a30e8a1cdf8052263bb4ee8a6cd2ae511
lizhichao12 <21301044@bjtu.edu.cn>
2023-11-08 01:06:49 +0800

    Update 后端

--- JPapp/src/main/java/com/bjtu/config/AuthAccess.java
+++ JPapp/src/main/java/com/bjtu/config/AuthAccess.java
@@ -2,16 +2,8 @@ package com.bjtu.config;
 
 import java.lang.annotation.*;
 
-<<<<<<< HEAD
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface AuthAccess {
-
-=======
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface AuthAccess {
->>>>>>> main
 }
--- JPapp/src/main/java/com/bjtu/controller/UserController.java
+++ JPapp/src/main/java/com/bjtu/controller/UserController.java
@@ -3,24 +3,6 @@ package com.bjtu.controller;
 import com.bjtu.config.AuthAccess;
 import com.bjtu.pojo.RspObject;
 import com.bjtu.pojo.User;
-<<<<<<< HEAD
-import com.bjtu.service.EmailService;
-import com.bjtu.service.UserService;
-import org.apache.ibatis.annotations.Result;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
-
-
-import org.springframework.web.bind.annotation.*;
-import com.bjtu.util.Utils;
-
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.rmi.ServerException;
-import java.util.List;
-=======
 import com.bjtu.service.AdminService;
 import com.bjtu.service.StudentService;
 import com.bjtu.service.TeacherService;
@@ -39,7 +21,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import com.bjtu.exception.ServiceException;
->>>>>>> main
 
 
 @RestController
@@ -59,18 +40,10 @@ public class UserController {
     @Autowired
     private HttpSession session;
 
-<<<<<<< HEAD
-
-    @PostMapping("login")
-    public RspObject<User> login( String username,  String password){
-
-        System.out.println(username+" "+password);
-=======
     @AuthAccess
     @PostMapping("login")
     public RspObject<User> login(String username, String password, String role){
         System.out.println(username+" "+password+" "+role);
->>>>>>> main
 
 //        异常捕获
         Assert.hasLength(username,"用户名不能为空！");
@@ -88,22 +61,6 @@ public class UserController {
         }
     }
 
-<<<<<<< HEAD
-
-    @PostMapping("email")
-    public RspObject<Object> register(HttpServletRequest request,@RequestParam String username,@RequestParam String email) {
-        try {
-            // 生成验证码
-            String code = Utils.generateVerificationCode();
-
-            session.setAttribute("code",code);
-            session.setAttribute("username",username);
-//            System.out.println(email);
-//            System.out.println(code);
-//            System.out.println(1);
-            emailService.sendSimpleMessage(email, "注册验证码", "您的验证码是：" + code);
-//            System.out.println(2);
-=======
     @AuthAccess
     @PostMapping("email")
     public RspObject<Object> email(String email) {
@@ -112,7 +69,6 @@ public class UserController {
             String code = Utils.generateVerificationCode();
             session.setAttribute("vcode",code);
             emailService.sendSimpleMessage(email, "注册验证码", "您的验证码是：" + code);
->>>>>>> main
             return RspObject.success("验证码已发送至您的邮箱");
         } catch (Exception e) {
 //            return RspObject.fail("验证码未发送至您的邮箱");
