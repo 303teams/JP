23559e340a12b56b98876b784f28cb2787540044
Zengquan Liang <21301042@bjtu.edu.cn>
2023-11-09 13:18:31 +0800

    updata 前端

--- JPapp_vue/src/components/InfoPage.vue
+++ JPapp_vue/src/components/InfoPage.vue
@@ -2,48 +2,44 @@
   <div>
     <el-card class="box-card">
       <el-descriptions class="margin-top" title="简介" :column="1" border>
-        <el-descriptions-item label="账户名">2112</el-descriptions-item>
-        <el-descriptions-item label="昵称">我</el-descriptions-item>
-        <el-descriptions-item label="年龄">13</el-descriptions-item>
-        <el-descriptions-item label="性别">
-          <el-tag size="small">{{ sex }}</el-tag>
-        </el-descriptions-item>
-        <el-descriptions-item label="邮箱">
-          {{ email }}
-        </el-descriptions-item>
+        <el-descriptions-item label="账号">{{ id }}</el-descriptions-item>
+        <el-descriptions-item label="名字">{{ name }}</el-descriptions-item>
+        <el-descriptions-item label="性别">{{ sex }}</el-descriptions-item>
+        <el-descriptions-item label="邮箱">{{ email }}</el-descriptions-item>
+        <el-descriptions-item label="年龄">{{ age }}</el-descriptions-item>
       </el-descriptions>
       <el-row class="button-row">
         <el-col :span="24">
-          <el-button type="primary" size="small">操作</el-button>
+          <el-button type="primary" size="small" @click="Modifyinfo">修改</el-button>
         </el-col>
       </el-row>
     </el-card>
+    <ModifyInfo  ref="dia" @flesh="reload"></ModifyInfo>
   </div>
 </template>
 
 
 <script>
+import {mapState} from "vuex";
+import ModifyInfo from "@/HomeManagement/ModifyInfo.vue";
+
 export default {
+  components: {ModifyInfo},
   name: "InfoPage",
 
   data() {
-    return {
-      id: Number,
-      username: String,
-      age: Number,
-      email: String,
-      mobilePhoneNumber: String,
-      sex: String,
-    };
-  },
-  mounted() {
-    this.load();
+    return {};
   },
   methods: {
-    load() {
-
+    Modifyinfo() {
+      this.$refs.dia.open();
     },
   },
+
+  computed:{
+     ...mapState(['id', 'name', 'sex', 'email', 'age'])
+  },
+
 };
 </script>
 
