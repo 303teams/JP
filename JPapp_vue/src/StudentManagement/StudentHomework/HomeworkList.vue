0ea3b04ec239410312a2d98b94c8cb302434e23a
lizhichao12 <21301044@bjtu.edu.cn>
2023-11-24 12:59:32 +0800

    Update 后端

--- JPapp_vue/src/StudentManagement/StudentHomework/HomeworkList.vue
+++ JPapp_vue/src/StudentManagement/StudentHomework/HomeworkList.vue
@@ -25,7 +25,7 @@
         <el-table-column label="课程名称" sortable prop="courseName" />
         <el-table-column label="发布人" sortable prop="teacherName" />
         <el-table-column label="截止时间" width="200px" sortable prop="submitDdl" />
-        <el-table-column label="互评任务" width="200px" sortable prop="submitDdl" />
+        <el-table-column label="互评任务" width="200px" sortable prop="scoreDdl" />
         <el-table-column label="提交作业">
           <template v-slot="scope">
           <el-button size="large" v-if="scope.row.contentID === null" @click="handleSubmit(props.cno, scope.row.homeworkID)">提交</el-button>
