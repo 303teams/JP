f020670881d6329d59daabe28fe9fc2c811a0f81
lizhichao12 <21301044@bjtu.edu.cn>
2023-12-18 12:57:42 +0800

    Update

--- JPapp/src/main/java/com/bjtu/pojo/Content.java
+++ JPapp/src/main/java/com/bjtu/pojo/Content.java
@@ -21,7 +21,7 @@ public class Content implements Serializable {
     private String fileName;
 
 //    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Timestamp submitTime;
 
     private Integer score;
