22d7bf17af0904b86b9a4707f0e53e4c36c29256
Zengquan Liang <21301042@bjtu.edu.cn>
2023-12-20 18:35:20 +0800

    update

--- JPapp_vue/src/TeacherManagement/TeacherHomework/ReviewDetails.vue
+++ JPapp_vue/src/TeacherManagement/TeacherHomework/ReviewDetails.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <el-dialog title="重复列表" :close-on-click-modal="false" :lock-scroll="false" v-model="DetailsDialogVis">
+    <el-dialog title="查重名单" :close-on-click-modal="false" :lock-scroll="false" v-model="DetailsDialogVis">
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column label="学号" prop="id" width="180"/>
-        <el-table-column label="姓名" prop="name" width="150"/>
-        <el-table-column label="重复率" prop="" width="150"/>
+        <el-table-column label="学号" prop="sno"/>
+        <el-table-column label="姓名" prop="sname"/>
+        <el-table-column label="重复率" prop="similar"/>
+        <el-table-column label="作业提交内容" align="center" width="150px">
+          <template v-slot="scope">
+          <el-button type="text" @click="DownloadCT(scope.row)">查看作业</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-dialog>
   </div>
@@ -40,6 +45,42 @@ const fetchData = (value) => {
         console.log(err);
       });
 };
+
+const DownloadCT = (row) =>{
+  const data = {
+    contentID: row.contentID,
+  };
+  http.downloadCT(data)
+      .then(res => {
+        console.log(res);
+        const blob = new Blob([res.data], { type: 'application/octet-stream' });
+        const blobUrl = URL.createObjectURL(blob);
+
+        const fileName = ref('');
+        // 提取 filename 的方法
+        function extractFilename(contentDisposition) {
+          const matches = contentDisposition.match(/filename="(.+?)"/);
+          return matches ? decodeURIComponent(matches[1]) : null;
+        }
+
+        const contentDisposition = res.headers['content-disposition'];
+        fileName.value = extractFilename(contentDisposition);
+
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = fileName.value;
+
+        // 模拟点击下载
+        link.click();
+
+        // 释放 URL 对象
+        URL.revokeObjectURL(blobUrl);
+      })
+      .catch(err => {
+        console.error("发生未知错误！");
+        console.log(err);
+      });
+}
 
 defineExpose({
   openDetailDialog
